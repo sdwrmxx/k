12bc8c76bba4e4a9fc3b03761af48ebd388bde29
Роман <112051061+ShadowRoma25@users.noreply.github.com>
2024-08-01 01:31:02 +0500

    update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let isChanged = false;
+document.getElementById('changeColorButton').addEventListener('click', function() {
+	if (isChanged == false) {
+		document.getElementById('changeColorText').style.color = "#ffffff";
+	isChanged = true;
+	} else {
+		document.getElementById('changeColorText').style.color = "#ff00ff";
+	isChanged = false;
+	}
+}); 
